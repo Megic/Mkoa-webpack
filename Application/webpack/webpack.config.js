40db2d287903c6a07c5c001778398465c8c09b73
megic <420636220@qq.com>
2016-05-02 19:23:22 +0800

    webpack打包模块

---
+++ Application/webpack/webpack.config.js
@@ -0,0 +1,64 @@
+// Mkoawebpack配置
+var path = require("path");
+var fs = require('fs');
+var webpack = require('webpack');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var _ = require('underscore');//辅助函数
+var sConfig = require('../../Mkoa/config')(path.join(__dirname, '../../'));
+var userConfig = require('../../config/config')(path.join(__dirname, '../../'));
+_.extend(sConfig, userConfig);
+var $C = sConfig;
+
+module.exports = {
+    'entry': getFiles(), //加载入口文件
+    module: {
+        loaders: [//各类文件处理器
+            {test: /\.js$/, loader: "babel"},
+            {test: /\.css$/, loader:ExtractTextPlugin.extract("style-loader", "css-loader")},
+            {test: /\.json$/,   loader: 'json'},
+            {test: /\.(jpg|png)$/, loader: "url?limit=8192"}
+        ]
+    },
+    output: {
+        path: $C.staticpath+$C.V,//打包输出的路径
+        filename: '[name]' //打包后的名字
+    },
+    resolveLoader: {
+        modulesDirectories: [
+            __dirname+'/node_modules'
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('common.js'),//公共代码
+        new ExtractTextPlugin("common.css", {allChunks: true})//公共样式
+    ],
+    resolve: {
+        root:  path.join(__dirname, 'lib')//公共库文件夹
+    }
+};
+
+
+function getFiles(){
+    //加载Mkoa各模块下webpack目录文件作为入口文件
+    var libArr={};
+    var apppath=$C.ROOT+ '/' +$C.application;
+    function walk(apppath,callback){
+        var dirList = fs.readdirSync(apppath);
+        dirList.forEach(function(item){
+            if(fs.statSync(apppath + '/' + item).isDirectory()){
+                walk(apppath + '/' + item);
+            }else{
+                callback(apppath  + '/' + item,item);
+            }});
+    }
+    var moudelList = fs.readdirSync(apppath);
+    moudelList.forEach(function(item){
+        if(fs.statSync(apppath + '/' + item).isDirectory()){
+            var mdPath=apppath + '/' + item+ '/webpack/entry/' ;//加载入口文件
+            if(fs.existsSync(mdPath)) walk(mdPath,function(filePath,fileName){
+                var name=filePath.replace(apppath,'').replace('/webpack/entry/','');
+                libArr[name]=filePath;
+            });
+        }});
+    return libArr;
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Mkoa-webpack
-Mkoa webpack打包方案
+Mkoa webpack打包模块
+
+>Mkoa各模块增加webpack文件夹，里面再创建entry，entry放入口文件,建议建立webpack/lib文件夹放入口文件所需的其他js文件
+
+>webpack自动将Mkoa各模块目录下webpack/entry目录里面的入口文件进行打包输出
+
+>说明有点抽象，见例子
+
+----
+
+>npm install webpack -g 全局安装webpack
+
+>进入模块目录Application\webpack 运行webpack 对各个模块的入口文件进行打包。统一输出到静态文件夹
+
+>更多说明参考webpack
+
